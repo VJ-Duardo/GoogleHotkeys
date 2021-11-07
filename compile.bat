a17d197be8db7e9e7fbcf213d443430b44e40436
VJ-Duardo <olafstein501@gmail.com>
2021-11-07 12:58:41 +0100

    improved compile script

--- compile.bat
+++ compile.bat
@@ -2,12 +2,15 @@ set dir_name="GoogleHotkeys-0.3.7"
 
 set au3exe_path="C:\Program Files (x86)\AutoIt3\AutoIt3.exe"
 set au3wr_path="C:\Program Files (x86)\AutoIt3\SciTE\AutoIt3Wrapper\AutoIt3Wrapper.au3"
-set au2ex_path="C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe"
 
 
 if not exist %dir_name% (
-mkdir %dir_name%/
+  mkdir %dir_name%/
+) else (
+  del %dir_name% /s /f /q
 )
+
+xcopy settings.txt %dir_name%
 
 
 %au3exe_path% %au3wr_path% /NoStatus /prod /in "GoogleHotkeys.au3"
